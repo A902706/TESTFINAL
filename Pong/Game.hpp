9630383f55036ac80fdcc275001031c327740e84
A902706 <Sanchezandres5746@gmail.com>
2024-12-05 22:26:17 -0800

    test commit

--- Pong/Game.hpp
+++
@@ -1,9 +0,0 @@
-#include "Player.hpp"
-
-class Game {
-
-public:
-	void run(sf::RenderWindow& window, Player& player, sf::Font &font, sf::Sprite shapeArray[], sf::Text gameOver, sf::Sprite sprite);
-	void create();
-
-};
